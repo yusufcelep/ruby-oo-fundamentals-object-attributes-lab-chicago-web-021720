4d5c3ebf1477b37214a0caf8917ddf57f267f48e
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-19 22:54:41 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog 
+  attr_accessor :name, :breed 
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def breed=(breed)
+    @breed = breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,11 @@
+class Person
+  attr_accessor :name, :job 
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def job=(job)
+    @job = job
+  end
+end
